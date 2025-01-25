42cb35fa61ad056ded0f602bebe52a4816ca0924
Tibira Botto <tibira.botto@gmail.com>
2025-01-25 14:49:25 -0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,7 @@ const app = express();
 
 const isDev = process.env.NODE_ENV === "development";
 
-app.use(
-  cors({
-    origin: isDev
-      ? "http://localhost:3000"
-      : "https://prenotes-web.up.railway.app",
-    allowedHeaders: ["Content/Type"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 app.use("/v1/api/prenotes", prenoteRoutes);
 app.use("/v1/api/articles", articlesRoutes);
