3adf7a18f6fad0a34ce6d4d5bf900315f95a51b6
Tibira Botto <tibira.botto@gmail.com>
2025-01-30 20:55:32 -0500

    .

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 import express from "express";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import morgan from "morgan";
 import { errorHandler } from "./middlewares/errorHandler";
 import prenoteRoutes from "./routes/prenoteRoutes";
 import articlesRoutes from "./routes/articleRoutes";
 import chartsRoutes from "./routes/chartsRoutes";
 import deleteRoutes from "./routes/deleteRoutes";
+
 const app = express();
+const server = http.createServer(app); // Manter o servidor HTTP
+
+// Configuração do socket.io para usar o servidor HTTP
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(
   morgan(function (tokens, req, res) {
@@ -37,9 +46,38 @@ app.use("/", (req, res) => {
 });
 app.use(errorHandler);
 
+io.on("connection", (socket) => {
+  console.log("Cliente conectado: >>>>>>>>>>>>>>>>>>>>", socket.id);
+
+  socket.on("updateInProgress", async (cardId) => {
+    console.log(`Card ${cardId} atualizado para "em progresso"`);
+    await prisma.article.update({
+      where: { id: Number(cardId) },
+      data: { status: "in_progress" },
+    });
+
+    io.emit("progressUpdated");
+  });
+
+  socket.on("updateAvailable", async (cardId) => {
+    console.log(`Card ${cardId} atualizado para "em available"`);
+    await prisma.article.update({
+      where: { id: Number(cardId) },
+      data: { status: "available" },
+    });
+
+    io.emit("progressUpdated");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado:", socket.id);
+  });
+});
+
 const port = 5000;
 const host = "0.0.0.0";
 
-app.listen(port, host, () => {
+// Usando server.listen() em vez de app.listen()
+server.listen(port, host, () => {
   console.log(`Servidor rodando em http://${host}:${port}`);
 });
